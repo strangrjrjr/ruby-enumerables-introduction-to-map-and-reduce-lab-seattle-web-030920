8fbf37ae399a4e13a5b0a509b09a563c52e79054
Roger Robertson Jr <rjrjr@pm.me>
2020-02-01 18:44:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,34 @@
 # My Code here....
+def map_to_negativize(source)
+  
+end
+
+def map_to_no_change(source)
+  result = []
+  counter = 0
+  while counter < source.length
+    result[counter] = (source[counter] - (2 * source[counter]))
+    counter += 1
+  end
+  return source
+end
+
+def map_to_double(source)
+  
+end
+
+def map_to_square(source)
+  
+end
+
+def reduce_to_total(source, start)
+  
+end
+
+def reduce_to_all_true(source)
+  
+end
+
+def reduce_to_any_true(source)
+  
+end

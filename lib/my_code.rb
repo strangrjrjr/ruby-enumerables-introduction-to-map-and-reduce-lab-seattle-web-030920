141b34b95f99cfcc9ae8c3315f44abc9af2d6b29
Roger Robertson Jr <rjrjr@pm.me>
2020-02-01 19:14:21 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,56 @@
 # My Code here....
+def map_to_negativize(source)
+  result = []
+  source.each do |num|
+    result << (num * -1)
+  end
+  result
+end
+
+def map_to_no_change(source)
+  source
+end
+
+def map_to_double(source)
+  result = []
+  source.each do |num|
+    result << (num * 2)
+  end
+  result
+end
+
+def map_to_square(source)
+  result = []
+  source.each do |num|
+    result << (num * num)
+  end
+  result
+end
+
+def reduce_to_total(source, start = 0)
+  total = start
+  source.each do |num|
+    total += num
+  end
+  total
+end
+
+def reduce_to_all_true(source)
+  result = true
+  source.each do |value|
+    if !value
+      result = false
+    end
+  end
+  result
+end
+
+def reduce_to_any_true(source)
+  result = false
+  source.each do |value|
+    if value
+      result = true
+    end
+  end
+  result
+end
